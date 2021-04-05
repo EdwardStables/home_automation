f67f24bb03a3b22cd35b9d66018ae81b1b5b9962
Edward Stables <edward.stables1198@gmail.com>
2021-04-05 23:12:32 +0100

    tidied up build script

--- clients/build.sh
+++ clients/build.sh
@@ -9,15 +9,12 @@ fi
 
 [ -f $SKETCH/secrets.h ] && rm $SKETCH/secrets.h
 
-grep -ve "^\#" *.secret | grep "\S" | cut -d# -f1 | \
+grep -vhe "^\#" *.secret | grep "\S" | cut -d# -f1 | \
     awk '{print "#define " $1 " \"" $2"\""}' > $SKETCH/secrets.h
 
 FQBN=`grep FQBN build_config | awk '{print $2}'`
 PORT=`grep PORT build_config | awk '{print $2}'`
 
-echo $FQBN
-echo $PORT
-
-arduino-cli compile --fqbn $FQBN $SKETCH
+arduino-cli compile -b $FQBN $SKETCH
 [ $? != "0" ] && exit 1
-arduino-cli upload -p $PORT --fqbn $FQBN $SKETCH
+arduino-cli upload -p $PORT -b $FQBN $SKETCH
